81f977251fb05ed69bc034fae054b3de4c431060
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-01-16 17:52:36 -0300

    Create l1-q3.py
    O programa deve ler três strings quaisquer e imprimir a soma dessas strings duas a duas, em todas as ordens possíveis.

---
+++ lista1/l1-q3.py
@@ -0,0 +1,12 @@
+# 3 - Somando strings
+
+str1 = input()
+str2 = input()
+str3 = input()
+
+print(str1 + str2)
+print(str1 + str3)
+print(str2 + str1)
+print(str2 + str3)
+print(str3 + str1)
+print(str3 + str2)
