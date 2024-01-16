73349831a67c6d4e1701e3ec3ee52388563a4aac
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-01-16 18:06:58 -0300

    Create l1-q1.py

---
+++ lista1/l1-q1.py
@@ -0,0 +1,7 @@
+# 1 - Papagaio Engraçado
+'''No parque ecológico de São Carlos há um papagaio engraçado. Ele adora aprender palavras novas, mas sempre as repete duas vezes.  As crianças de divertem demais!
+Seu programa deve imitar o papagaio engraçado >:-)'''
+
+word = input()
+print(word)
+print(word)
