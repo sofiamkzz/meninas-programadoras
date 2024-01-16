2419127a54ed14169691c65d9ac325a54b4ac180
sofiamkzz <124911331+sofiamkzz@users.noreply.github.com>
2024-01-16 18:13:01 -0300

    Create l3-q1.py

---
+++ lista3/l3-q1.py
@@ -0,0 +1,6 @@
+# 1 - Contador A
+'''O programa deve ler um valor inteiro positivo e imprimir, um por linha, os valores entre 0 e o valor anterior ao inteiro lido.'''
+
+valor = int(input())
+for item in range(valor):
+    print(item)
